ca7f71cefa1f5b8686b253f507fe375db2b14954
Will Browning <wdbrwnng@memphis.edu>
2024-03-22 22:55:27 -0500

    Problem 1, having troubles getting to work

---
+++ problem1.rb
@@ -0,0 +1,9 @@
+def extract_urls(text)
+  pattern = '/(?:https?|ftp):\/\/[\w\-]+(?:\.[\w\-]+)+[\w\-.,@?^=%&:/~\+#]*[\w\-@?^=%&/~\+#]/'
+  text.scan(pattern)
+end
+
+sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
+
+urls = extract_urls(sample_text)
+urls.each { |url| puts url }
